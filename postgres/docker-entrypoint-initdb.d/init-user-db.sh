a3b6da613102f1767ed1350b349a9edccd0bb0cf
Glyn Bowden <glyn.bowden@hpe.com>
2019-01-02 13:12:19 +0000

    Added Postgres Deployment and Usecase integration

---
+++ postgres/docker-entrypoint-initdb.d/init-user-db.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_usecase;
+    GRANT ALL PRIVILEGES ON DATABASE oai_usecase TO oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_build;
+    GRANT ALL PRIVILEGES ON DATABASE oai_build TO oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_environment;
+    GRANT ALL PRIVILEGES ON DATABASE oai_environment TO oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_datasource;
+    GRANT ALL PRIVILEGES ON DATABASE oai_datasource TO oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_components;
+    GRANT ALL PRIVILEGES ON DATABASE oai_components TO oneai_db;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE oai_keycloak;
+    GRANT ALL PRIVILEGES ON DATABASE oai_keycloak TO oneai_db;
+EOSQL
+
